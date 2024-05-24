8e12887817e0f85f38654844fdcb95befc4e7d19
FABRICA <luis.farid@outlook.com>
2024-05-23 19:44:22 -0600

    hielos en el vaso

--- scripts/index.js
+++ scripts/index.js
@@ -10,23 +10,6 @@ let hielo;
 
 
 
-// tamanoButtons.forEach(button => {
-//     button.addEventListener('click', (event) => {
-//       tamano = event.target.innerText;
-//       console.log(`Tamaño seleccionado: ${tamano}`);
-//     });
-//   });
-  
-  
-
-  // hieloButtons.forEach(button => {
-  //   button.addEventListener('click', (event) => {
-  //     hielo = event.target.innerText;
-  //     console.log(`Hielo seleccionado: ${hielo}`);
-  //   });
-  // });
-
-
   document.addEventListener('DOMContentLoaded', () => {
     const tamanoButtons = document.querySelectorAll('.btn-size-ch, .btn-size-md, .btn-size-g');
     const modal = document.getElementById('modal');
@@ -77,8 +60,49 @@ let hielo;
         componente.className = 'glass'
         break
 
-
-
     }
   }
+
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const hieloButtons = document.querySelectorAll('.btn-ice-yes, .btn-ice-no');
+    const modal = document.getElementById('modal');
+    const modalText = document.getElementById('modal-text');
+    const modalAccept = document.getElementById('modal-accept');
+    const modalCancel = document.getElementById('modal-cancel');
+    const iceContainer = document.getElementById('ice-container');
+  
+    let selectedHielo = '';
+  
+    hieloButtons.forEach(button => {
+      button.addEventListener('click', (event) => {
+        selectedHielo = event.target.innerText;
+        modalText.innerText = `Opción seleccionada: ${selectedHielo}`;
+        modal.style.display = 'block';
+      });
+    });
+  
+    modalAccept.addEventListener('click', () => {
+      modal.style.display = 'none';
+  
+      hielo = selectedHielo;
+      if (selectedHielo === 'Agregar hielo') {
+        iceContainer.style.display = 'flex'
+      } else {
+        iceContainer.style.display = 'none'
+      }
+    });
+  
+    modalCancel.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+  
+    window.addEventListener('click', (event) => {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
   
